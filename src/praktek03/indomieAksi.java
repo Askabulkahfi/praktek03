ba28d3a88a73ec245ea84b194c3a3dd2cc8140ee
akhmad03 <37820079+akhmad03@users.noreply.github.com>
2018-04-08 21:46:07 +0800

    menggunakan method set

--- src/praktek03/indomieAksi.java
+++ src/praktek03/indomieAksi.java
@@ -2,9 +2,10 @@ package praktek03;
 public class indomieAksi {
     public static void main(String[] args) {
         indomie A = new indomie();
-        A.merk="sarimi";
-        A.rasa="soto";
-        A.harga=3000;
+        
+        A.setMerk("sedaap");
+        A.setRasa("ayampanggang");
+        A.setHarga(2000);
         
         A.cetakinfo();
     }
