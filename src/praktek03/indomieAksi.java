e25a59b22118253b5fe2a92e764b810f1991f216
akhmad03 <37820079+akhmad03@users.noreply.github.com>
2018-04-08 21:30:40 +0800

    instance buat atribut dan panggil method

--- src/praktek03/indomieAksi.java
+++ src/praktek03/indomieAksi.java
@@ -1,7 +1,12 @@
 package praktek03;
 public class indomieAksi {
     public static void main(String[] args) {
-    
+        indomie A = new indomie();
+        A.merk="sarimi";
+        A.rasa="soto";
+        A.harga=3000;
+        
+        A.cetakinfo();
     }
     
 }
