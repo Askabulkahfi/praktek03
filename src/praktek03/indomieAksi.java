868fffe14e0d9e6b2d6584afca5b6d43d0b7c35f
akhmad03 <37820079+akhmad03@users.noreply.github.com>
2018-04-08 22:28:41 +0800

    selesai

--- src/praktek03/indomieAksi.java
+++ src/praktek03/indomieAksi.java
@@ -6,7 +6,7 @@ public class indomieAksi {
         A.setMerk("sedaap");
         A.setRasa("ayampanggang");
         A.setHarga(2000);
-        
+        A.setJenisgoreng();
         A.cetakinfo();
         
         System.out.print("Merknya \t: ");
@@ -15,6 +15,15 @@ public class indomieAksi {
         System.out.println(A.getRasa());
         System.out.print("Harganya \t: ");
         System.out.println(A.getHarga());
+        
+        indomiegoreng dm= new indomiegoreng();
+        dm.setMerk("sakura");
+        dm.setRasa("ayampedas");
+        dm.setHarga(4000);
+        dm.setJenisgoreng("goreng");
+        dm.cetakinfo();
+        System.out.println("Jenis :dm.getJenisgoreng();
+        dm.throttle();
     }
     
 }
