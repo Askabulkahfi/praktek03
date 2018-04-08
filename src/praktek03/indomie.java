790583b87efd510e5008c0813f80ce32d0a9acb7
akhmad03 <37820079+akhmad03@users.noreply.github.com>
2018-04-08 21:38:47 +0800

    perbaikan dan buat Getter and Setter

--- src/praktek03/indomie.java
+++ src/praktek03/indomie.java
@@ -7,9 +7,35 @@ public class indomie {
     private double harga;
     
     void cetakinfo(){
-        System.out.prntln("Merk \t: "+merk+"\n"+
-                "warna \t: "+rasa+"\n"+
-                "warna \t: "+harga+"\n"+
+        System.out.println("Merk \t: "+merk+"\n"+
+                "Rasa \t: "+rasa+"\n"+
+                "Harga \t: "+harga+"\n");
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getRasa() {
+        return rasa;
+    }
+
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
     
 }
