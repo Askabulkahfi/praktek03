e645ff67603e89708422f781f47876d2daea3557
akhmad03 <37820079+akhmad03@users.noreply.github.com>
2018-04-08 22:01:10 +0800

    atribut dan method

--- src/praktek03/indomiegoreng.java
+++ src/praktek03/indomiegoreng.java
@@ -2,5 +2,10 @@ package praktek03;
 
 
 public class indomiegoreng extends indomie {
+    private String jenisgoreng;
+    
+    void throttle(){
+        System.out.println("pedas");
+    }
     
 }
